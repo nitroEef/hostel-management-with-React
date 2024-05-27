9f32678f82773a42a64a62ec577ad39f20e3113f
nitroEef <mikebodetelli@gmail.com>
2024-05-27 15:27:29 +0100

    update

--- backend/models/roomModel.js
+++ backend/models/roomModel.js
@@ -1,2 +1,26 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs');
+
+const roomSchema = mongoose.Schema({
+    roomNumber: {
+        type: number,
+        required: true
+    },
+    capacity: {
+        type: number,
+        required: true
+    },
+    occupancy: {
+        type: number,
+        required: true
+    },
+
+    location: {
+        type: string,
+        required: true
+    },
+    status: {
+        type: string,
+        required: true
+    }
+})
