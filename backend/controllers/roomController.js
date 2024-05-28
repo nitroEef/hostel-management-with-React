8db37d5a077ee0f1bda60c7554fe46db40ca8049
nitroEef <mikebodetelli@gmail.com>
2024-05-28 14:09:48 +0100

    update

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -1,4 +1,3 @@
-
 const asyncHandler = require("express-async-handler");
 const Room = require("../models/roomModel");
 
@@ -34,48 +33,91 @@ const createNewRoom = asyncHandler(async(req, res) => {
 
 });
 
-const getAllRooms = asyncHandler(async (req, res) => {
-const getrooms = await Room.find().sort()
-    
- if (!getrooms) {
-      res.status(500);
-      throw new Error("something went wrong");
+//Get all rooms
+const getAllRooms = asyncHandler(async(req, res) =>{
+    const rooms = await Room.find().sort();
+    if(!rooms) {
+        res.status(500)
+        throw new Error("Something went wrong")
     }
-    
-    res.status(200).json(getrooms);
-  });
 
+    res.status(200).json(rooms)
+});
 
-  const getRoom = asyncHandler(async(req,res) => {
+// Get single room
+const getRoom = asyncHandler(async(req, res) => {
+
     const roomId = req.params.roomId;
-    try{
-      const room = await Room.findById(roomId);
-      if(room){
-        const{_id, roomNumber, roomCapacity, roomOccupancy, roomLocation,  roomStatus} = room;
-        res.status(200).json({_id, roomNumber, roomCapacity, roomOccupancy, roomLocation,  roomStatus})
-      }else{
-        res.status(404).json("yaasin room is not found")
-      }
+    try {
+        const room = await Room.findById(roomId);
+        
+        if (room) {
+            const {_id, roomNumber, roomCapacity, roomOccupancy, roomLocation,  roomStatus } = room;
+            res.status(200).json({_id, roomNumber, roomCapacity, roomOccupancy, roomLocation,  roomStatus})
+        } else {
+            res.status(404).json({message: "Room not Found"})
+        }
+       
+    }catch (error) {
+        res.status(500).json({message: "Internal server Error"})
     }
-    catch(error){
-      res.status(500).json("Server Error");
-    }})
+})
 
-// to update room 
-    const updateRoom = asyncHandler(async(req,res) =>{}
-      const roomId = req.params.roomId;
-      
-  )
+//Update room
+const updateRoom = asyncHandler(async(req, res) => {
+    const roomId = req.params.roomId;
+    
+    try {
+        const room = await Room.findById(roomId);
 
+        if(room) {
+            const {_id, roomNumber, roomCapacity, roomOccupancy, roomLocation,  roomStatus } = room;
 
+            room.roomNumber = req.body.roomNumber || roomNumber;
+            room.roomCapacity = req.body.roomCapacity || roomCapacity;
+            room.roomOccupancy = req.body.roomOccupancy || roomOccupancy;
+            room.roomLocation = req.body.roomLocation || roomLocation;
+            room.roomStatus = req.body.roomStatus || roomStatus;
 
+            const updatedRoom = await room.save();
 
+            res.status(200).json({
+                _id: updatedRoom._id,
+                roomNumber: updatedRoom.roomNumber, 
+                roomCapacity: updatedRoom.roomCapacity, 
+                roomOccupancy: updatedRoom.roomOccupancy, 
+                roomLocation: updatedRoom.roomLocation, 
+                roomStatus: updatedRoom.roomStatus, 
 
+            })
+        }else {
+            res.status(404)
+            throw new Error("Room not Found")
+        }
 
+    } catch (error) { 
+        res.status(500).json({message: "Internal server error"})
+    }
+})
 
+//Delete room
+//Delete Room
+const deleteRoom = asyncHandler(async (req, res) => {
+  
+    const { roomId } = req.params
 
+    const room = Room.findById(roomId);
+    if (!room) {
+        res.status(404);
+        throw new Error("room not found in database");
 
+    }
 
+    await room.deleteOne();
+    res.status(200).json({
+        message: "room deleted successfully!"
+})
+}
+)
 
-
-module.exports = {createNewRoom, getAllRooms, getRoom} 
+module.exports = {createNewRoom, getAllRooms, getRoom, updateRoom, deleteRoom }
