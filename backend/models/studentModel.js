ac5f08faddbc450086e8663a3f0c74b87d24ad68
nitroEef <mikebodetelli@gmail.com>
2024-05-28 15:49:56 +0100

    update

--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -4,4 +4,14 @@ const bcrypt = require('bcryptjs');
 
 
 const guardianSchema = mongoose.Schema({
-    guar
+    guardianName :{
+        type: String,
+        required: true
+    },
+    guardianEmail: {
+        type: String,
+        required: [true, 'please add an email'],
+        trim: true
+    })
+
+    
