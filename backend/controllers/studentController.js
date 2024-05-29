74a329c669655d0fbd7d111b729dc5dd258d468a
nitroEef <mikebodetelli@gmail.com>
2024-05-29 14:43:38 +0100

    update

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -3,87 +3,97 @@ const Student = require("../models/studentModel");
 const Room = require("../models/roomModel");
 const {generateUniqueId} = require("../utils/generateUniqueId");
 
-const ensureUniqueId = async () =>{
+const ensureUniqueId = async () => {
     let uniqueId;
     let idExists = true;
-
-    while (idExists){
-        uniqueId = generateUniqueId();
-        const existingStudent = await Student.findById( uniqueId);
-        idExists = existingStudent;
+  
+    while (idExists) {
+      uniqueId = generateUniqueId();
+      const existingStudent = await Student.findById(uniqueId);
+      idExists = !!existingStudent;
     }
-
+  
     return uniqueId;
-}
+  };
+  
+  const registerStudent = asyncHandler(async (req, res) => {
+    try {
+      const { email, name, age, nationality, g_name, g_email, gender, roomNum } =
+        req.body;
+  
+      if (
+        !email ||
+        !name ||
+        !age ||
+        !nationality ||
+        !g_name ||
+        !g_email ||
+        !gender ||
+        !roomNum
+      ) {
+        res.status(400);
+        throw new Error("Please fill in all the required fields.");
+      }
+  
+      const studentExist = await Student.findOne({ email });
+  
+      if (studentExist) {
+        return res.status(400).json({ msg: "Student already exists" });
+      }
+  
+      const room = await Room.findOne({ roomNumber: roomNum });
+  
+      if (!room) {
+        return res.status(404).json({ msg: "Room not found" });
+      }
+  
+      if (room.roomStatus !== "available") {
+        return res.status(400).json({ msg: "Room is not available" });
+      }
+  
+      const uniqueId = await ensureUniqueId();
+  
+      const student = await Student.create({
+        _id: uniqueId,
+        email,
+        name,
+        age,
+        nationality,
+        guardian: {
+          guardianName: g_name,
+          guardianEmail: g_email,
+        },
+        gender,
+        room: room._id, // Assign the room's ObjectId to the student
+      });
+  
+      room.roomOccupancy.push(student._id);
+  
+      if (room.roomOccupancy.length >= room.roomCapacity) {
+        room.roomStatus = "unavailable";
+      }
+  
+      await room.save();
+  
+      res.status(201).json(student);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  });
 
-const registerStudent = asyncHandler(async (req, res) => {
-  try {
-    const { email, name, age, nationality, g_name, g_email, gender, roomNum } =
-      req.body;
+//   to get all students 
+const getAllStudent = asyncHandler(async (req, res) => {
+    const students = await Student.find().sort("-createdAt");
 
-    if (
-      !email ||
-      !name ||
-      !age ||
-      !nationality ||
-      !g_name ||
-      !g_email ||
-      !gender ||
-      !roomNum
-    ) {
-      res.status(400);
-      throw new Error("Please fill in all the required fields.");
+    if (!students) {
+      res.status(500);
+      throw new Error("Something went wrong");
     }
 
-    const studentExist = await Student.findOne({ email });
-
-    if (studentExist) {
-      return res.status(400).json({ msg: "Student already exists" });
-    }
-
-    const room = await Room.findOne({ roomNumber: roomNum });
-
-    if (!room) {
-      return res.status(404).json({ msg: "Room not found" });
-    }
-
-    if (room.roomStatus !== "available") {
-      return res.status(400).json({ msg: "Room is not available" });
-    }
-
-    const uniqueId = await ensureUniqueId();
-
-    const student = await Student.create({
-      _id: uniqueId,
-      email,
-      name,
-      age,
-      nationality,
-      guardian: {
-        guardianName: g_name,
-        guardianEmail: g_email,
-      },
-      gender,
-      room: room._id,
-      checkedIn: true,
-    });
-
-    room.roomOccupancy.push(student._id);
-
-    if (room.roomOccupancy.length >= room.roomCapacity) {
-      room.roomStatus = "unavailable";
-    }
-
-    await room.save();
-
-    res.status(201).json(student);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
+    res.status(200).json(students);
+  
 });
-
-const getAllStudent = asyncHandler(async (req, res) => {});
 const getStudent = asyncHandler(async (req, res) => {});
 const updateStudentProfile = asyncHandler(async (req, res) => {});
 const changeStudentRoom = asyncHandler(async (req, res) => {});
