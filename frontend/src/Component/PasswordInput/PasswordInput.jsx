4e5e75badd6685fb80f311d80c6aff6f536bc096
nitroEef <mikebodetelli@gmail.com>
2024-06-03 16:08:05 +0100

    update

--- frontend/src/Component/PasswordInput/PasswordInput.jsx
+++ frontend/src/Component/PasswordInput/PasswordInput.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './PasswordInput.css'
 import {AiOutlineEye, AiOutlineEyeInvisible} from 'react-icons/ai'
 
-const PasswordInput = ({placeholder, valye, onChange, name, Onpaste}) => {
+const PasswordInput = ({placeholder, value, onChange, name, onPaste}) => {
     const [showPassword, setShowPassword] = useState(false);
     const togglePassword = () => {
         setShowPassword(!showPassword);
@@ -10,8 +10,19 @@ const PasswordInput = ({placeholder, valye, onChange, name, Onpaste}) => {
 
   return (
     <div className='password'>
-        <input/>
+        <input
+        type={showPassword ? "text" : "password"} 
+        placeholder={placeholder}
+        required
+        name = {name}
+        onChange={onChange}
+        onPaste={onPaste
+        }
+        className='input'  />
       
+      <div className='icon' onClick={togglePassword}>
+        {showPassword ? <AiOutlineEyeInvisible size={20}/> : <AiOutlineEye size={20}/>}
+      </div>
     </div>
   )
 }
--- frontend/src/Component/Register/AdminReg.jsx
+++ frontend/src/Component/Register/AdminReg.jsx
@@ -1,10 +1,93 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import "./Register.css";
 import Loader from "../Loader/Loader";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import PasswordInput from "../PasswordInput/PasswordInput";
+import { UserContext } from "../../../context/userContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { FaTimes } from "react-icons/fa";
+import { BsCheck2All } from "react-icons/bs";
 
 const AdminReg = () => {
   const [loading, setLoading] = useState(true);
+  const { setUser } = useContext(UserContext);
+  const [formValidMessage, setFormValidMessage] = useState(false);
+  const [formCompleted, setFormCompleted] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formData, setFormData] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const [uCase, setUCase] = useState(false);
+  const [num, setNum] = useState(false);
+  const [sChar, setSChar] = useState(false);
+  const [passLength, setPassLength] = useState(false);
+
+  const navigate = useNavigate();
+
+  const timesIcon = <FaTimes color="red" size={20} />;
+  const checkIcon = <BsCheck2All color="green" size={20} />;
+
+  const switchIcon = (condition) => {
+    return condition ? checkIcon : timesIcon;
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    const { password } = formData;
+
+    setUCase(/([a-z].[A-Z])|([A-Z].[a-z])/.test(password));
+    setNum(/[0-9]/.test(password));
+    setSChar(/[!,%,&,@,#,$,^,*,?,_,~]/.test(password));
+    setPassLength(password.length > 5);
+  }, [formData.password]);
+
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+
+    const { fullname, email, password, password2 } = formData;
+
+    if (!fullname || !email || !password || !password2) {
+      setFormValidMessage("Oops!! All fields are required😵");
+      return;
+    }
+
+    if (password !== password) {
+      setFormValidMessage("Password does not match!");
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    axios
+      .post("http://localhost:3500/admin/register", formData)
+      .then((response) => {
+        setUser(response.data);
+        setIsSubmitting(false);
+        setFormCompleted(true);
+        toast.success("Registration Successful");
+        navigate("/homedash", { state: { user: response.data } });
+      })
+      .catch((error) => {
+        setIsSubmitting(false);
+        const message =
+          error.response?.status === 400
+            ? "A user with the same email already exists"
+            : "Server error, unable to register";
+        setFormValidMessage(message);
+      });
+  });
 
   useEffect(() => {
     setTimeout(() => {
@@ -20,50 +103,90 @@ const AdminReg = () => {
         <div className="container form__ --100vh">
           <div className="form-container">
             <p className="title">Create an account</p>
-            <form className="form">
+
+            <form className="form" onSubmit={handleSubmit}>
               <div className="--dir-column">
-                <label htmlFor="name">Full name:</label>
+                <label htmlFor="fullname">Full name:</label>
                 <input
                   type="text"
                   className="input"
-                  name="name"
+                  name="fullname"
                   placeholder="Enter your full name"
                   required
+                  value={formData.fullname}
+                  onClick={handleInputChange}
                 />
               </div>
 
               <div className="--dir-column">
-                <label htmlFor="name">Email:</label>
+                <label htmlFor="email">Email:</label>
                 <input
                   type="email"
                   className="input"
                   name="email"
                   placeholder="Enter your email"
                   required
+                  value={formData.email}
+                  onChange={handleInputChange}
                 />
               </div>
 
               <div className="--dir-column">
-                <label htmlFor="name">Password:</label>
-                <input
-                  type="password"
-                  className="input"
+                <label htmlFor="password">Password:</label>
+                <PasswordInput
+                  placeholder="password"
                   name="password"
-                  placeholder="Enter your password"
-                  required
+                  value={formData.password}
+                  onChange={handleInputChange}
                 />
               </div>
 
               <div className="--dir-column">
-                <label htmlFor="name">Confirm password:</label>
-                <input
-                  type="password"
-                  className="input"
-                  name="password"
-                  placeholder="Re-enter your password"
-                  required
+                <label htmlFor="password2">Confirm password:</label>
+                <PasswordInput
+                  placeholder="password"
+                  name="password2"
+                  value={formData.password2}
+                  onChange={handleInputChange}
+                  onPaste={(e) => {
+                    e.preventDefault();
+                    toast.error("Cannot paste into input field");
+                    return false;
+                  }}
                 />
               </div>
+                  
+              <div className="card">
+              <ul className="form-list">
+                <li>
+                  <span className="indicator">
+                    {switchIcon(uCase)} &nbsp; Lowercase & Uppercase
+                    </span>
+                </li>
+
+                <li>
+                  <span className="indicator">
+                    {switchIcon(passLength)} &nbsp; Number(0-9)
+                    </span>
+                </li>
+
+                <li>
+                  <span className="indicator">
+                    {switchIcon(sChar)} &nbsp; special character(!@#$%&*)
+                    </span>
+                </li>
+
+                
+                <li>
+                  <span className="indicator">
+                    {switchIcon(passLength)} &nbsp; at least 6 characters
+                    </span>
+                </li>
+              </ul>
+              </div>
+
+
+
               <button className="--btn">Create account</button>
             </form>
             <p>
