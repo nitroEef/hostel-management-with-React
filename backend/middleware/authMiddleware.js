50e2d8e68d717dcafd32d3ab67290a805857bdeb
nitroEef <mikebodetelli@gmail.com>
2024-06-03 09:43:23 +0100

    update

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,35 +1,37 @@
 const jwt = require("jsonwebtoken");
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
 const Admin = require("../models/AdminModel");
 
-const protect = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler(async(req, res, next) => {
     let token;
+
     if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      try {
-        token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.admin = await Admin.findById(decoded.id).select("-password");
+        req.headers.authorization && req.headers.authorization.startsWith("Bearer")
+    ){
+        try{
+            token = req.headers.authorization.split(" ")[1];
 
-        if(!req.user){
-            res.status(401);
-            throw new Error("Not authorized, user not found");
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await Admin.findById(decode.id).select("-password");
+
+            if(!req.user) {
+                res.status(401);
+                throw new Error("Not authorized, user not found");
+            }
+
+            next()
+        } catch (error) {
+            console.error(error)
+            res.status(401)
+            throw new Error("Not authorized, token failed")
         }
-        next();
-    
-      } catch (error) {
-        console.error(error);
+    }
+
+    if (!token){
         res.status(401);
-        throw new Error("Not authorized, token failed");
-      }
-  
-  }
-        if(!token){
-            res.status(401);
-            throw new Error("Not authorized, no token");
-        }
+      throw new Error("Not authorized, no token")
+    }
+})
 
-});
 module.exports = {protect}
